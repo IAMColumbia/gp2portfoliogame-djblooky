61e0483ad00245c5a96ec26ce4cf68c43e71d0a4
Haley Phillips <haleyphillips928@gmail.com>
2020-04-20 17:48:11 -0500

    windows strategy pattern

---
+++ src/Assets/Scripts/Windows/IWindow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IWindow 
+{
+    public string name { get; set; }
+    WindowState _state { get; set; }
+    public WindowState State { get; set; }
+
+    void Log(string s);
+}
