85924d96b40cf86457fe38e1b261651c7b388724
Haley Phillips <haleyphillips928@gmail.com>
2020-05-13 20:03:27 -0500

    music manager singleton

---
+++ src/Assets/Scripts/Music/MusicPlayerSingleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayerSingleton : MonoBehaviour
+{
+    public static MusicPlayerSingleton Instance { get; private set; }
+
+    public AudioClip [] values;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
